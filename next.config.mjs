200bf39ca6aaad8532fc8613493607c9ba266291
JustDevRae <ksrae165@gmail.com>
2024-07-23 19:36:32 +0900

    husky pre-commit 테스트

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	swcMinify: true, // SWC를 사용하여 코드 축소를 활성화
+	reactStrictMode: true, // React 엄격 모드 설정
+};
+
+export default nextConfig;
