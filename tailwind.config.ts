db21beebc658dd17bf553289023a94937b1e9e67
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-07-24 22:40:05 +0900

    chore: St 7 palette 테일윈드 palette 추가, 프리티어 import 자동정렬 plugin 추가
    * ST-7-chore: 테일윈드 palette 추가
    
    * ST-7-chore: 프리티어 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,7 +38,39 @@ const config: Config = {
       borderRadius: px0_100,
       width: px0_500,
       height: px0_500,
-      colors: {},
+      colors: {
+        black: "#1B1B1B",
+        "nomad-black": "#323236",
+        gray: {
+          800: "#484848",
+          700: "#79747E",
+          600: "#A4A1AA",
+          500: "#ADAE88",
+          400: "#CBC9CF",
+          300: "#DDDDDD",
+          200: "#EEEEEE",
+          100: "#FAFAFA",
+        },
+        green: {
+          DEFAULT: "#0B332D",
+          light: "#F1FFFD",
+          bright: "#00A007",
+        },
+        red: {
+          DEFAULT: "#FF472E",
+          light: "#FFF4E8",
+        },
+        orange: {
+          DEFAULT: "#FF7D1D",
+          light: "#FFF4E8",
+        },
+        yellow: "#FFC23D",
+        blue: {
+          DEFAULT: "#0086FF",
+          light: "#42E8FF",
+          lighter: "#12FBFF",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
