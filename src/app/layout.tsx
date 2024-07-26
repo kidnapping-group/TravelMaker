0c9deef583001fe12600c121677afc0bf073605b
Darkthos <kiki7951@naver.com>
2024-07-26 17:28:00 +0900

    ST-35-feat: 모든 font 색깔 #b1b1b1 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/app/_components/Header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -17,7 +18,10 @@ function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} text-black`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
