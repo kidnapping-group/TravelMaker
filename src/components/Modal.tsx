011ad5d175265839d7dea2c5d8037130554ffe93
Darkthos <kiki7951@naver.com>
2024-07-29 03:17:37 +0900

    ST-13-chore: small사이즈 이미지 크기와 font css 수정

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,29 +11,31 @@ interface ModalProps {
 }
 
 const sizeConfig = {
-  small: { width: "tablet:w-[368px]", padding: "tablet:px-5" },
-  normal: { width: "tablet:w-[430px]", padding: "tablet:px-6" },
-  large: { width: "tablet:w-[490px]", padding: "tablet:px-6" },
+  small: { width: "tablet:w-[368px]", padding: "tablet:px-5", imgSize: 24, fontSize: "text-xl" },
+  normal: { width: "tablet:w-[430px]", padding: "tablet:px-6", imgSize: 40, fontSize: "text-28px" },
+  large: { width: "tablet:w-[490px]", padding: "tablet:px-6", imgSize: 40, fontSize: "text-28px" },
 };
-
 let modalToggle: React.Dispatch<React.SetStateAction<boolean>> | null = null;
 
 function Modal({ children, title, size = "normal", bg = "white" }: ModalProps) {
   const [isOpen, setIsOpen] = useState(false);
-
   modalToggle = setIsOpen;
 
   if (!isOpen) return null;
-
-  const { width, padding } = sizeConfig[size];
+  const { width, padding, imgSize, fontSize } = sizeConfig[size];
 
   return (
     <div className="fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-70">
       <div className={`h-full w-full bg-${bg} shadow-lg tablet:h-auto ${width} tablet:rounded-lg`}>
         <div className={`flex items-center justify-between px-4 pt-4 ${padding} tablet:pt-6`}>
-          <h1 className="text-xl font-semibold">{title}</h1>
+          <h1 className={`${fontSize} font-bold`}>{title}</h1>
           <button type="button" onClick={() => setIsOpen(false)}>
-            <Image width={40} height={40} alt="창 닫기" src="/icons/icon-close-black.svg" />
+            <Image
+              width={imgSize}
+              height={imgSize}
+              alt="창 닫기"
+              src="/icons/icon-close-black.svg"
+            />
           </button>
         </div>
         {children}
