eb1e6d0b76cbeda7c8394d4cd85a863d9a003740
Darkthos <kiki7951@naver.com>
2024-07-29 20:57:10 +0900

    ST-11-fix: 대문자 버그 수정 2/2

---
+++ src/app/test/Dropdown.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FocusEvent, useRef, useState } from "react";
+
+interface MenuItem {
+  title: string;
+  status: string;
+}
+
+interface DropdownProps {
+  menuItems: MenuItem[];
+  type: "dropdown" | "selector" | "timeSelector";
+}
+
+const styleConfig = {
+  dropdown: {
+    container: "w-auto text-lg font-medium",
+    button: "rounded-2xl text-green border-green p-4",
+    item: "border-b text-center py-4 h-14 text-[#4B4B4B]",
+    selectedText: "text-green",
+    dropdownList: "",
+    image: { style: "dropdown-small", size: 16 },
+  },
+  selector: {
+    container: "w-full text-base font-light",
+    button: "rounded-[4px] text-[#A1A1A1] border-gray-500 py-4 pl-4",
+    item: "text-left pl-2 py-2 h-10",
+    selectedText: "text-gray-600",
+    dropdownList: "p-2",
+    image: { style: "dropdown", size: 48 },
+  },
+  timeSelector: {
+    container: "w-full text-base font-light",
+    button: "rounded-[4px] text-[#A1A1A1] border-gray-500 py-4 pl-4",
+    item: "text-left pl-2 py-2 h-10",
+    selectedText: "",
+    dropdownList: "p-2",
+    image: { style: "dropdown", size: 48 },
+  },
+};
+
+function Dropdown({ menuItems, type = "dropdown" }: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(menuItems[0]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+
+  if (!menuItems.length) return null;
+
+  const handleItemClick = (item: MenuItem) => {
+    setSelectedItem(item);
+    setIsOpen(false);
+    if (type !== "selector") router.push(`?status=${encodeURIComponent(item.status)}`);
+  };
+
+  const handleBlur = (e: FocusEvent<HTMLElement>) => {
+    if (!dropdownRef.current?.contains(e.relatedTarget)) {
+      setIsOpen(false);
+    }
+  };
+
+  const styles = styleConfig[type];
+  const itemsToRender = type !== "dropdown" ? menuItems.slice(1) : menuItems;
+
+  return (
+    <div>
+      <div
+        className={`relative inline-block min-w-32 ${styles.container}`}
+        ref={dropdownRef}
+        onBlur={handleBlur}
+      >
+        <button
+          type="button"
+          className={`flex h-14 w-full items-center justify-between gap-2 whitespace-nowrap border ${styles.button} text-left shadow-sm`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span className={styles.selectedText}>{selectedItem.title}</span>
+          <Image
+            src={`/icons/icon-${isOpen ? "dropup" : styles.image.style}.svg`}
+            alt="드롭다운 버튼"
+            width={styles.image.size}
+            height={styles.image.size}
+          />
+        </button>
+        {isOpen && (
+          <div
+            className={`absolute ${styles.dropdownList} mt-2 w-full rounded-md border border-[#1122110D] bg-white shadow-sm`}
+          >
+            {itemsToRender.map(item => (
+              <button
+                className={`block w-full ${styles.item} rounded-md hover:bg-gray-200`}
+                type="button"
+                key={item.status}
+                onClick={() => handleItemClick(item)}
+              >
+                {item.title}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,5 +1,16 @@
 "use server";
 
+import Dropdown from "@/app/test/Dropdown";
+
+const menuItems = [
+  { title: "전체 예약", status: "" },
+  { title: "예약 신청", status: "pending" },
+  { title: "예약 취소", status: "confirmed" },
+  { title: "예약 승인", status: "declined" },
+  { title: "예약 거절", status: "canceled" },
+  { title: "체험 완료", status: "completed" },
+];
+
 async function Page({ searchParams }: { searchParams: { status: string } }) {
   let selectedStatus = searchParams.status;
   // if (status) {
@@ -7,7 +18,12 @@ async function Page({ searchParams }: { searchParams: { status: string } }) {
   // } else {
   //   await getActivity();
   // }
-  return <div>드롭 다운 조지기! selectedStatus: {selectedStatus}</div>;
+  return (
+    <div>
+      드롭 다운 조지기! selectedStatus: {selectedStatus}
+      <Dropdown menuItems={menuItems} type="dropdown" />
+    </div>
+  );
 }
 
 export default Page;
