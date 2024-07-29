f03b1b6017b580aed4cda43a0eb28fdfee1e09cf
Darkthos <kiki7951@naver.com>
2024-07-29 20:55:55 +0900

    ST-11-fix: 대문자 버그 수정 1/2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,16 +1,5 @@
 "use server";
 
-import Dropdown from "@/app/components/Dropdown";
-
-const menuItems = [
-  { title: "전체 예약", status: "" },
-  { title: "예약 신청", status: "pending" },
-  { title: "예약 취소", status: "confirmed" },
-  { title: "예약 승인", status: "declined" },
-  { title: "예약 거절", status: "canceled" },
-  { title: "체험 완료", status: "completed" },
-];
-
 async function Page({ searchParams }: { searchParams: { status: string } }) {
   let selectedStatus = searchParams.status;
   // if (status) {
@@ -18,12 +7,7 @@ async function Page({ searchParams }: { searchParams: { status: string } }) {
   // } else {
   //   await getActivity();
   // }
-  return (
-    <div>
-      드롭 다운 조지기! selectedStatus: {selectedStatus}
-      <Dropdown menuItems={menuItems} type="dropdown" />
-    </div>
-  );
+  return <div>드롭 다운 조지기! selectedStatus: {selectedStatus}</div>;
 }
 
 export default Page;
