4c7597546042f61754d25563f10b17bf2d189302
Darkthos <kiki7951@naver.com>
2024-07-30 23:29:29 +0900

    ST-4-feat: Popup로직만 구현 css는 미완성

---
+++ src/app/test/page.tsx
@@ -0,0 +1,21 @@
+import Popup, { openPopup } from "@/app/components/Popup";
+import { redirect } from "next/navigation";
+
+function Page({ searchParams }) {
+  const { confirm } = searchParams;
+
+  if (confirm) {
+    redirect("/signin");
+  }
+  return (
+    <div>
+      test
+      <button type="button" onClick={openPopup}>
+        팝업 버튼
+      </button>
+      <Popup type="reservation" />
+    </div>
+  );
+}
+
+export default Page;
