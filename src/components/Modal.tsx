2a53e5409bb9e8d58d6b43eb23e148cc81d105d5
Darkthos <kiki7951@naver.com>
2024-07-30 22:18:15 +0900

    ST-13-chore: 타입 수정및 잡다한 스타일 제거

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,58 +1,27 @@
 "use client";
 
 import Image from "next/image";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 
-interface ModalProps {
-  children: ReactNode;
+interface ModalProps extends React.PropsWithChildren {
   title: string;
-  size?: "small" | "normal" | "large";
-  bg?: string;
 }
 
-const sizeConfig = {
-  small: {
-    width: "tablet:w-[368px]",
-    padding: "tablet:px-5 tablet:pt-6 px-5 pt-10",
-    imgSize: 24,
-    fontSize: "text-xl",
-  },
-  normal: {
-    width: "tablet:w-[430px]",
-    padding: "tablet:px-6 tablet:pt-6 px-4 pt-4",
-    imgSize: 40,
-    fontSize: "text-28px",
-  },
-  large: {
-    width: "tablet:w-[490px]",
-    padding: "tablet:px-6 tablet:pt-6 px-4 pt-4",
-    imgSize: 40,
-    fontSize: "text-28px",
-  },
-};
 let modalToggle: React.Dispatch<React.SetStateAction<boolean>> | null = null;
 
-function Modal({ children, title, size = "normal", bg = "white" }: ModalProps) {
+function Modal({ children, title }: ModalProps) {
   const [isOpen, setIsOpen] = useState(false);
   modalToggle = setIsOpen;
 
   if (!isOpen) return null;
-  const { width, padding, imgSize, fontSize } = sizeConfig[size];
 
   return (
-    <div
-      className={`fixed inset-0 z-20 flex items-center justify-center ${bg === "white" && "bg-black bg-opacity-70"}`}
-    >
-      <div className={`h-full w-full bg-${bg} shadow-lg tablet:h-auto ${width} tablet:rounded-lg`}>
-        <div className={`flex items-center justify-between ${padding}`}>
-          <h1 className={`${fontSize} font-bold`}>{title}</h1>
+    <div className="fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-70">
+      <div className="h-full w-full bg-white shadow-lg tablet:h-auto tablet:w-[430px] tablet:rounded-lg">
+        <div className="flex items-center justify-between px-4 pt-4 tablet:px-6 tablet:pt-6">
+          <h1 className="text-28px font-bold">{title}</h1>
           <button type="button" onClick={() => setIsOpen(false)}>
-            <Image
-              width={imgSize}
-              height={imgSize}
-              alt="창 닫기"
-              src="/icons/icon-close-black.svg"
-            />
+            <Image width={40} height={40} alt="창 닫기" src="/icons/icon-close-black.svg" />
           </button>
         </div>
         {children}
