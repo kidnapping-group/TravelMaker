d10a54ac18fbf21437986aefb60f731ce72bdd8d
heony704 <heony704@gmail.com>
2024-08-02 00:20:57 +0900

    ST-45-chore: tailwind custom font size 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,6 +44,16 @@ const config: Config = {
         lighter: "#12FBFF",
       },
     },
+    fontSize: {
+      xs: ["12px", "18px"],
+      sm: ["13px", "22px"],
+      md: ["14px", "24px"],
+      lg: ["16px", "26px"],
+      "2lg": ["18px", "26px"],
+      xl: ["20px", "32px"],
+      "2xl": ["24px", "32px"],
+      "3xl": ["32px", "42px"],
+    },
   },
 };
 export default config;
