746187e85b5753eb9800763fa1c731a9dcbdecca
heony704 <heony704@gmail.com>
2024-08-02 00:08:42 +0900

    ST-45-refactor: tailwind 커스텀 스타일 extend 말고 기본으로 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,43 +7,41 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        pc: "1200px",
-        tablet: "768px",
+    screens: {
+      pc: "1200px",
+      tablet: "768px",
+    },
+    colors: {
+      black: "#1B1B1B",
+      "nomad-black": "#323236",
+      gray: {
+        800: "#484848",
+        700: "#79747E",
+        600: "#A4A1AA",
+        500: "#ADAE88",
+        400: "#CBC9CF",
+        300: "#DDDDDD",
+        200: "#EEEEEE",
+        100: "#FAFAFA",
       },
-      colors: {
-        black: "#1B1B1B",
-        "nomad-black": "#323236",
-        gray: {
-          800: "#484848",
-          700: "#79747E",
-          600: "#A4A1AA",
-          500: "#ADAE88",
-          400: "#CBC9CF",
-          300: "#DDDDDD",
-          200: "#EEEEEE",
-          100: "#FAFAFA",
-        },
-        green: {
-          DEFAULT: "#0B332D",
-          light: "#F1FFFD",
-          bright: "#00A007",
-        },
-        red: {
-          DEFAULT: "#FF472E",
-          light: "#FFF4E8",
-        },
-        orange: {
-          DEFAULT: "#FF7D1D",
-          light: "#FFF4E8",
-        },
-        yellow: "#FFC23D",
-        blue: {
-          DEFAULT: "#0086FF",
-          light: "#42E8FF",
-          lighter: "#12FBFF",
-        },
+      green: {
+        DEFAULT: "#0B332D",
+        light: "#F1FFFD",
+        bright: "#00A007",
+      },
+      red: {
+        DEFAULT: "#FF472E",
+        light: "#FFF4E8",
+      },
+      orange: {
+        DEFAULT: "#FF7D1D",
+        light: "#FFF4E8",
+      },
+      yellow: "#FFC23D",
+      blue: {
+        DEFAULT: "#0086FF",
+        light: "#42E8FF",
+        lighter: "#12FBFF",
       },
     },
   },
