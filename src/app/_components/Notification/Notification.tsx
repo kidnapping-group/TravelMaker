7c3e4531c46df870901bc3813b32afbaf20785bf
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-04 01:24:28 +0900

    ST-10-fix: pr 반영 수정

---
+++ src/app/_components/Notification/Notification.tsx
@@ -0,0 +1,58 @@
+import AlertItem from "@/app/_components/Notification/AlertItem";
+import Image from "next/image";
+
+export type NotificationListResponseData = {
+  totalCount: number;
+  notifications: Array<Notifications>;
+  cursorId: null;
+};
+export type Notifications = {
+  id: number;
+  teamId: string;
+  userId: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null;
+};
+
+interface NotificationModalProps {
+  data: NotificationListResponseData;
+  onClose: () => void;
+}
+
+function Notification({ data, onClose }: NotificationModalProps) {
+  const notifications = data?.notifications || [];
+
+  return (
+    <div className="fixed inset-0 z-50 flex tablet:relative pc:relative">
+      <div className="overflow relative h-full w-full bg-gray-400 px-[14px] py-6 tablet:absolute tablet:top-12px tablet:h-[494px] tablet:w-[368px] tablet:rounded-[10px] pc:absolute">
+        <div className="flex h-full w-full flex-col gap-4">
+          <div className="flex items-center justify-between">
+            <p className="text-20px font-bold">알림 {data.totalCount}개</p>
+            <Image
+              className="cursor-pointer"
+              src="/icons/icon-close-black.svg"
+              width={24}
+              height={24}
+              alt="닫기"
+              onClick={onClose}
+            />
+          </div>
+          <div className="flex flex-col gap-2 overflow-y-auto">
+            {notifications.map(notification => (
+              <AlertItem key={notification.id} notification={notification} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Notification;
+
+
+
+
+
