f20d5ef768ffd92723eeb81ce3ecb90e8e0a1995
JustDevRae <ksrae165@gmail.com>
2024-08-03 12:05:27 +0900

    ST-8-refactor: PasswordInput 기능 Input에 병합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,57 @@
+"use client";
+
+import Input from "@/components/Input/Input";
+import baseSchema from "@/utils/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type LoginFormData = z.infer<typeof baseSchema>;
+const loginSchema = baseSchema.pick({ email: true, password: true });
+
 function Home() {
-  return <div>Home</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, touchedFields },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+    mode: "all",
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex w-[400px] flex-col gap-[10px]">
+        <Input
+          register={register("email")}
+          type="email"
+          name="email"
+          label="이메일"
+          placeholder="이메일을 입력해주세요"
+          error={errors.email}
+          touched={touchedFields.email}
+        />
+
+        <Input
+          register={register("password")}
+          type="password"
+          name="password"
+          label="비밀번호"
+          placeholder="비밀번호를 입력해주세요"
+          error={errors.password}
+          touched={touchedFields.password}
+        />
+        <button
+          type="submit"
+          disabled={!isValid}
+          className="h-[45px] w-full rounded-[10px] bg-blue-500 text-[14px] font-semibold leading-[24px] text-white hover:bg-blue-700 disabled:bg-gray-300"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 export default Home;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import { useState } from "react";
 import { FieldError, UseFormRegisterReturn } from "react-hook-form";
 
-import PasswordInput from "./PasswordInput";
+import OffEye from "../../../public/icons/icon-eye-off.svg";
+import Eye from "../../../public/icons/icon-eye.svg";
 
 interface InputProps extends React.PropsWithChildren {
   register: UseFormRegisterReturn;
@@ -21,6 +24,8 @@ export default function Input({
   error,
   touched,
 }: InputProps) {
+  const [showPassword, setShowPassword] = useState(false);
+
   const getClassName = () => {
     if (error) {
       return "border border-red";
@@ -31,30 +36,26 @@ export default function Input({
     return "";
   };
 
-  if (type === "password") {
-    return (
-      <PasswordInput
-        register={register}
-        name={name}
-        label={label}
-        placeholder={placeholder}
-        error={error}
-        touched={touched}
-      />
-    );
-  }
   return (
-    <div className="flex flex-col gap-[8px]">
+    <div className="relative flex flex-col gap-[10px]">
       <label htmlFor={name} className="text-[16px] font-normal leading-[26px] text-black">
         {label}
       </label>
       <input
         id={name}
-        type={type}
+        type={type === "password" && showPassword ? "text" : type}
         placeholder={placeholder}
         {...register}
         className={`h-[58px] w-full rounded-[6px] bg-gray-200 py-[10px] pl-[10px] pr-[40px] outline-none ${getClassName()}`}
       />
+      {type === "password" && (
+        <Image
+          src={showPassword ? Eye : OffEye}
+          alt="toggle visibility"
+          className="absolute right-[10px] top-[53px] cursor-pointer"
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      )}
       {error && <p className="text-[12px] font-normal leading-[18px] text-red">{error.message}</p>}
     </div>
   );
