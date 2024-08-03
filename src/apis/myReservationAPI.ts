cacd92b9ad4c921cdcddd249e25e93321f6c8c12
MountionRiver <162934516+JooKangsan@users.noreply.github.com>
2024-08-03 20:48:57 +0900

    feat: API 로직 구현(ST-14) (#22)
    * ST-14-feat: axios instance 추가
    
    * ST-14-feat: activitiesApi 작성
    
    * ST-14-fix: husky 수정
    
    * ST-14-feat: userAPI 작성
    
    * ST-14-feat: myReservationAPI작성
    
    * ST-14-feat: myNotificationsAPI 작성
    
    * ST-14-fix: intercepter 누락 코드 추가
    
    * ST-14-fix: activitiesAPI 타입 수정
    
    * ST-14-fix: usersApi 타입 수정
    
    * ST-14-fix: 내 얘약, 내 알림  타입 수정
    
    * ST-14-feat: myActivitiesAPI, myNotificationsAPI 작성
    
    * ST-14-refactor: interface -> type 으로 변경
    
    * ST-14-refactor: activitiesAPI.get method 기본값 지정
    
    * ST-14-refactor: 인터셉터 수정 및 api 검증및 에러나는 곳 refactor
    
    * ST-14-fix: local스토리지 쿠키로 변경
    
    * ST-14-refactor: env 설정
    
    * ST-14-refactor: env 파일삭제
    
    * ST-14-refactor: env 파일 추가

---
+++ src/apis/myReservationAPI.ts
@@ -0,0 +1,43 @@
+import {
+  ReservationId,
+  ReservationRes,
+  getReservation,
+  patchReservationRes,
+  postReviews,
+  postReviewsRes,
+} from "./API.type";
+import axiosInstance from "./axiosInstance";
+
+const myReservationAPI = {
+  // 내 예약 리스트 조회
+  get: async (params?: getReservation) => {
+    const { data } = await axiosInstance.get<ReservationRes>("/my-reservations", {
+      params: {
+        cursorId: params?.cursorId,
+        size: params?.size ?? 10,
+        status: params?.status,
+      },
+    });
+    return data;
+  },
+  // 내 예약 수정(취소)
+  patch: async (reservationId: number) => {
+    const response = await axiosInstance.patch<patchReservationRes>(
+      `/my-reservations/${reservationId}`,
+      {
+        status: "canceled",
+      },
+    );
+    return response.data;
+  },
+  // 내 예약 리뷰 작성
+  postReviews: async (reservationId: ReservationId, body: postReviews) => {
+    const response = await axiosInstance.post<postReviewsRes>(
+      `/my-reservations/${reservationId}/reviews`,
+      body,
+    );
+    return response.data;
+  },
+};
+
+export default myReservationAPI;
