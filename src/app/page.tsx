50edc95d0a17a5575a3a1b038e9c115c2752f810
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-08-05 20:32:10 +0900

    feat: react쿼리 라이브러리 설치및 초기 세팅(ST-49)  (#36)
    * ST-49-feat: react쿼리 라이브러리 설치및 초기 세팅
    
    * ST-49-chore: .env.local => .env
    
    * ST-49-remove: .env
    
    * ST-49-remove: 주석 삭제
    
    * ST-49-chore: 줄 추가
    
    * ST-49-fix: id
    
    * ST-49-remove: 쓸때없는 쿼리옵션 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-function Home() {
-  return <div>Home</div>;
+import activitiesAPI from "@/apis/activitiesAPI";
+import Link from "next/link";
+
+async function Home() {
+  const { activities } = await activitiesAPI.get();
+  return (
+    <div className="flex flex-col gap-10">
+      {activities.map(item => (
+        <Link href={`/${item.id}`} key={item.id}>
+          {item.title}
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
