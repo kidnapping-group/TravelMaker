056fb3747796e3aaae2e27bb2ee942129809ff7b
JustDevRae <ksrae165@gmail.com>
2024-08-05 18:17:00 +0900

    ST-8-chore: input 유틸함수 파일명 변경 및 위치 변경

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import getInputClassName from "@/utils/getInputClassName";
+import getInputColorStatus from "@/components/Input/getInputColorStatus";
 import Image from "next/image";
 import { useState } from "react";
 import { FieldError, UseFormRegisterReturn } from "react-hook-form";
@@ -26,7 +26,6 @@ export default function Input({
   touched,
 }: InputProps) {
   const [showPassword, setShowPassword] = useState(false);
-
   return (
     <div className="relative flex flex-col gap-[10px]">
       <label htmlFor={name} className="text-[16px] font-normal leading-[26px] text-black">
@@ -37,7 +36,7 @@ export default function Input({
         type={type === "password" && showPassword ? "text" : type}
         placeholder={placeholder}
         {...register}
-        className={`h-[58px] w-full rounded-[6px] bg-gray-200 py-[10px] pl-[10px] outline-none ${type === "password" ? "pr-[40px]" : "pr-[10px]"} ${getInputClassName(error, touched)}`}
+        className={`h-[58px] w-full rounded-[6px] bg-gray-200 py-[10px] pl-[10px] outline-none ${type === "password" ? "pr-[40px]" : "pr-[10px]"} ${getInputColorStatus(error, touched)}`}
       />
       {type === "password" && (
         <Image
--- src/components/Input/TextArea.tsx
+++ src/components/Input/TextArea.tsx
@@ -1,5 +1,6 @@
-import getInputClassName from "@/utils/getInputClassName";
 import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+import getInputColorStatus from "./getInputColorStatus";
 
 interface TextAreaProps extends React.PropsWithChildren {
   register: UseFormRegisterReturn;
@@ -16,7 +17,7 @@ export default function TextArea({ register, name, placeholder, error, touched }
         {...register}
         id={name}
         placeholder={placeholder}
-        className={`h-[200px] w-full resize-none rounded-[6px] bg-gray-200 px-[10px] py-[10px] outline-none ${getInputClassName(error, touched)}`}
+        className={`h-[200px] w-full resize-none rounded-[6px] bg-gray-200 px-[10px] py-[10px] outline-none ${getInputColorStatus(error, touched)}`}
       />
       {error && <p className="text-[12px] font-normal leading-[18px] text-red">{error.message}</p>}
     </div>
