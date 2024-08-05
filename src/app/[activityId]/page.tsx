19ffac54089b40822232d6dfa087c86a30f2db07
Darkthos <kiki7951@naver.com>
2024-08-05 20:24:29 +0900

    ST-49-fix: id

--- src/app/[activityId]/page.tsx
+++ src/app/[activityId]/page.tsx
@@ -2,7 +2,7 @@ import activitiesAPI from "@/apis/activitiesAPI";
 import Image from "next/image";
 
 async function ActivityId({ params: { activityId } }: { params: { activityId: string } }) {
-  const res = await activitiesAPI.getInfo({ id: Number(activityId) });
+  const res = await activitiesAPI.getInfo(Number(activityId));
 
   return (
     <div>
