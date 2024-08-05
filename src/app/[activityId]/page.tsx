95e0729dcc74f0cc13cb0a35964ccf8f2ea8094c
Darkthos <kiki7951@naver.com>
2024-08-05 19:16:46 +0900

    ST-49-remove: 주석 삭제

--- src/app/[activityId]/page.tsx
+++ src/app/[activityId]/page.tsx
@@ -2,9 +2,6 @@ import activitiesAPI from "@/apis/activitiesAPI";
 import Image from "next/image";
 
 async function ActivityId({ params: { activityId } }: { params: { activityId: string } }) {
-  // api id로 불러오고 시작해라 리액트쿼리 잘이용해보자 막막하다..
-  // 팀원에게 징징대보자
-
   const res = await activitiesAPI.getInfo({ id: Number(activityId) });
   return (
     <div>
