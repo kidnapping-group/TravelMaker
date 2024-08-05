b0011d98a64cd0c0fd71b8c9368ac06a98aeabf3
Darkthos <kiki7951@naver.com>
2024-08-05 16:29:07 +0900

    ST-25-chore: .env => .env.local

--- src/app/[activityId]/page.tsx
+++ src/app/[activityId]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 async function ActivityId({ params: { activityId } }: { params: { activityId: string } }) {
   // api id로 불러오고 시작해라 리액트쿼리 잘이용해보자 막막하다..
   // 팀원에게 징징대보자
+
   const res = await activitiesAPI.getInfo({ id: Number(activityId) });
   return (
     <div>
