e566f791b596c222e55cb2fc741a9b68fc45d728
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-08 04:22:37 +0900

    ST-55-feat: 내 체험관리 페이지 추가(임시)

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,22 @@
+import SideNavigation from "@/app/(user)/_components/SideNavigation";
+
+function UserRootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative bg-gray-100">
+      <div className="h-100vh fixed inset-0 box-border flex justify-center bg-gray-100 pt-[100px] tablet:pt-[72px]">
+        <div className="flex w-full max-w-[1230px] justify-center gap-6">
+          <div className="sticky top-[200px]">
+            <SideNavigation />
+          </div>
+          <div className="w-full overflow-auto">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserRootLayout;
