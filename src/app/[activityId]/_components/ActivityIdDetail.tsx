0d8df750bc2b3b0507f02d91b0018868fcde10f9
Darkthos <kiki7951@naver.com>
2024-08-08 09:28:57 +0900

    ST-57-feat: margin

--- src/app/[activityId]/_components/ActivityIdDetail.tsx
+++ src/app/[activityId]/_components/ActivityIdDetail.tsx
@@ -6,7 +6,7 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
 import { useState } from "react";
 
-// 시도해보자!! 이번에 map으로 만들어서 어케 잘 조정해보자
+// 반응형별 마진이랑 서브이미지 grid하자.
 function ActivityIdDetail({ activityId }: { activityId: string }) {
   const { data } = useSuspenseQuery(activityIdOptions(activityId));
   const [imageIndex, setImageIndex] = useState(0);
@@ -30,8 +30,8 @@ function ActivityIdDetail({ activityId }: { activityId: string }) {
           <p>{data.address}</p>
         </div>
       </div>
-      <div className="flex justify-center">
-        <div className="relative h-[310px] w-[375px]">
+      <div className="flex justify-center gap-1 tablet:justify-normal">
+        <div className="relative h-[310px] w-[375px] tablet:w-[345px] pc:h-[534px] pc:w-[595px]">
           <Image
             src={imageUrlArray[imageIndex]}
             alt={`${data.title} 배경 사진`}
@@ -57,31 +57,38 @@ function ActivityIdDetail({ activityId }: { activityId: string }) {
             <Image src="icons/icon-next.svg" alt="다음 사진" width={24} height={47} />
           </button>
         </div>
-        <div className="hidden tablet:block">
+        <div className="hidden tablet:grid tablet:grid-cols-2 tablet:grid-rows-2 tablet:gap-1">
           {data.subImages.map(item => (
-            <Image
-              key={item.id}
-              src={item.imageUrl}
-              alt={`${data.title} 서브 사진`}
-              width={170}
-              height={150}
-            />
+            <div key={item.id} className="relative h-[152px] w-[170px] pc:h-[264px] pc:w-[294px]">
+              <Image
+                src={item.imageUrl}
+                alt={`${data.title} 서브 사진`}
+                fill
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </div>
           ))}
         </div>
       </div>
-      <div className="mt-4 flex flex-col justify-center gap-4">
-        <div className="flex flex-col justify-center gap-4">
-          <p className="text-xl font-bold">체험 설명</p>
-          <p className="text-lg font-normal">{data.description}</p>
-        </div>
-        <div className="border-#112211 border" />
-        <div>
-          <Map address={data.address} />
-          <div className="flex items-center gap-1">
-            <Image src="icons/icon-location.svg" alt="위치 아이콘" width={18} height={18} />
-            <p>{data.address}</p>
+      <div className="flex gap-3 tablet:mt-8 pc:mt-20 pc:gap-6">
+        <div className="mt-4 flex flex-col justify-center gap-4 tablet:gap-10">
+          <div className="border-#112211 hidden border tablet:block" />
+          <div className="flex flex-col justify-center gap-4">
+            <p className="text-xl font-bold">체험 설명</p>
+            <p className="text-lg font-normal">{data.description}</p>
+          </div>
+          <div className="border-#112211 border" />
+          <div>
+            <Map address={data.address} />
+            <div className="mt-2 flex items-center gap-1">
+              <Image src="icons/icon-location.svg" alt="위치 아이콘" width={18} height={18} />
+              <p>{data.address}</p>
+            </div>
           </div>
         </div>
+        <div className="hidden h-auto tablet:block tablet:min-w-[252px] pc:min-w-[384px]" />
       </div>
     </div>
   );
