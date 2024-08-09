334934039820408e390645edf3b4bd77ddd22441
Darkthos <kiki7951@naver.com>
2024-08-09 13:44:22 +0900

    ST-25-feat: 캘린더 모달 만듬.

--- src/app/[activityId]/page.tsx
+++ src/app/[activityId]/page.tsx
@@ -1,6 +1,8 @@
 import ActivityIdDetail from "@/app/[activityId]/_components/ActivityIdDetail";
+import ReservationModal from "@/app/[activityId]/_components/ReservationModal";
 import Review from "@/app/[activityId]/_components/Review";
 import { activityIdOptions, reviewOptions } from "@/app/[activityId]/activityId";
+import { openModal } from "@/components/Modal";
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
 
 async function ActivityId({ params: { activityId } }: { params: { activityId: string } }) {
@@ -15,6 +17,14 @@ async function ActivityId({ params: { activityId } }: { params: { activityId: st
     <HydrationBoundary state={dehydrate(queryClient)}>
       <ActivityIdDetail activityId={activityId} />
       <Review activityId={activityId} />
+      <ReservationModal activityId={activityId} />
+      <button
+        onClick={openModal}
+        type="button"
+        className="sticky bottom-0 w-full bg-primary-400 py-5"
+      >
+        예약하기
+      </button>
     </HydrationBoundary>
   );
 }
