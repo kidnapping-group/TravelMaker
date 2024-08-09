cc758dd9078de477ba047963d7205369ef1f9016
heony704 <heony704@gmail.com>
2024-08-09 12:09:26 +0900

    ST-36-feat: 메인 페이지 배너 & 검색 창 UI 구현

---
+++ src/app/_components/SearchForm.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/Button";
+import Image from "next/image";
+
+interface Props {
+  placeholder: string;
+}
+
+function SearchForm({ placeholder }: Props) {
+  return (
+    <form className="flex gap-3">
+      <div className="relative flex grow items-center">
+        <Image
+          className="absolute"
+          src="/icons/bed.svg"
+          width={48}
+          height={48}
+          alt="검색 아이콘"
+          draggable={false}
+        />
+        <input
+          className="h-full w-full rounded-lg bg-gray-100 py-2 pl-12 pr-4 text-lg font-medium placeholder:text-gray-400"
+          placeholder={placeholder}
+        />
+      </div>
+      <Button size="large">검색</Button>
+    </form>
+  );
+}
+
+export default SearchForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
 import activitiesAPI from "@/apis/activitiesAPI";
 import Link from "next/link";
 
+import SearchForm from "./_components/SearchForm";
+
 async function Home() {
   const { activities } = await activitiesAPI.get();
+
   return (
-    <div className="flex flex-col gap-10">
+    <main>
+      <section className="flex w-full justify-center bg-black bg-opacity-50">
+        <div className="mx-5 flex w-full max-w-[1200px] flex-col gap-8 pb-[60px] pt-[100px]">
+          <div className="font-bold text-white">
+            <h2 className="text-3xl">
+              함께 배우면 즐거운
+              <br /> 스트릿 댄스
+            </h2>
+            <p className="text-xl">1월의 인기 체험 BEST 🔥</p>
+          </div>
+          <div className="flex flex-col gap-5 rounded-2xl bg-white p-5">
+            <h2 className="text-2lg font-bold">무엇을 체험하고 싶으신가요?</h2>
+            <SearchForm placeholder="내가 원하는 체험은" />
+          </div>
+        </div>
+      </section>
+
       {activities.map(item => (
         <Link href={`/${item.id}`} key={item.id}>
           {item.title}
         </Link>
       ))}
-    </div>
+    </main>
   );
 }
 
