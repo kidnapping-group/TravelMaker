e4e6e2ef3795c599d72f739957c03f973b7310d0
heony704 <heony704@gmail.com>
2024-08-09 10:23:59 +0900

    ST-4-feat: 테스트 페이지 수정

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -13,7 +13,8 @@ function Page({ searchParams }: { searchParams: { confirm: string } }) {
       <button type="button" onClick={openPopup}>
         팝업 버튼
       </button>
-      <Popup text="fdsavs" onCloseButton="확" />
+      <Popup text="일치하는 회원 정보가 없습니다." onCloseButton="확인" />
+      <Popup text="예약을 취소하시겠습니까?" onCloseButton="아니오" onChangeButton="취소" />
     </div>
   );
 }
