869947e85c50a748bcc29aaeef3306df3af98e2c
heony704 <heony704@gmail.com>
2024-08-12 04:31:14 +0900

    ST-36-feat: 검색창에 검색 기능 구현

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function SearchForm({ placeholder }: Props) {
   return (
-    <form className="flex gap-3">
+    <form className="flex gap-3" action="/search" method="get">
       <div className="relative flex grow items-center">
         <Image
           className="absolute"
@@ -19,10 +19,13 @@ function SearchForm({ placeholder }: Props) {
         />
         <input
           className="h-full w-full rounded-lg bg-gray-100 py-2 pl-12 pr-4 text-lg font-medium placeholder:text-gray-400"
+          name="keyword"
           placeholder={placeholder}
         />
       </div>
-      <Button size="large">검색</Button>
+      <Button size="large" type="submit">
+        검색
+      </Button>
     </form>
   );
 }
