14edfcf08538fb99d3c04adac848c8d34efe4261
heony704 <heony704@gmail.com>
2024-08-13 10:31:47 +0900

    ST-69-feat: 인기체험 10개 표시

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,35 @@
 import activitiesAPI from "@/apis/activitiesAPI";
+import Swiper from "@/components/Swiper";
 import Link from "next/link";
 
+import LongActivityCard from "./_components/LongActivityCard";
+
 async function Home() {
-  const { activities } = await activitiesAPI.get();
+  const { activities: popularActivities } = await activitiesAPI.get({
+    sort: "most_reviewed",
+    page: 1,
+    size: 10,
+  });
+
   return (
-    <div className="flex flex-col gap-10">
-      {activities.map(item => (
-        <Link href={`/${item.id}`} key={item.id}>
-          {item.title}
-        </Link>
-      ))}
-    </div>
+    <main className="m-auto w-full max-w-[1200px]">
+      <section className="flex flex-col">
+        <h2 className="py-3 text-2lg font-bold text-black">🔥 인기 체험</h2>
+        <Swiper>
+          {popularActivities.map(({ id, bannerImageUrl, title, price, rating, reviewCount }) => (
+            <Link href={`/${id}`} key={id}>
+              <LongActivityCard
+                bannerImageUrl={bannerImageUrl}
+                title={title}
+                price={price}
+                rating={rating}
+                reviewCount={reviewCount}
+              />
+            </Link>
+          ))}
+        </Swiper>
+      </section>
+    </main>
   );
 }
 
