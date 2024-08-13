eba0c0fe1aba4917c86834703442f1ffd2c01ce3
heony704 <heony704@gmail.com>
2024-08-14 06:06:19 +0900

    ST-70-feat: 체험 섹션들 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import activitiesAPI from "@/apis/activitiesAPI";
-import Link from "next/link";
+import ActivitySection from "@/app/_components/ActivitySection";
 
 async function Home() {
-  const { activities } = await activitiesAPI.get();
   return (
-    <div className="flex flex-col gap-10">
-      {activities.map(item => (
-        <Link href={`/${item.id}`} key={item.id}>
-          {item.title}
-        </Link>
-      ))}
-    </div>
+    <main className="m-auto w-full max-w-[1200px]">
+      <div className="flex flex-col gap-5">
+        <ActivitySection title="새로 오픈한 체험" sort="latest" />
+        <ActivitySection title="일상을 풍요롭게 만드는 특별한 경험 🎨🎶" category="arts" />
+        <ActivitySection title="입맛을 사로잡는 미식 여행 🍽️" category="food" />
+        <ActivitySection title="에너지 넘치는 스포츠 체험 ⚽" category="sports" />
+        <ActivitySection title="발걸음을 따라가는 새로운 이야기, 특별한 투어 🚶‍♂️" category="tour" />
+        <ActivitySection title="웰빙으로 건강한 일상 만들기 🌱" category="wellbeing" />
+        <ActivitySection title="손끝에 전해지는 짜릿한 손맛! 🐟" keyword="낚시" />
+      </div>
+    </main>
   );
 }
 
