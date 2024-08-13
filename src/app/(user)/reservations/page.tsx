cbfa209ef5b314a0c5cf798b4b68ca3d63e605f7
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-13 22:58:53 +0900

    ST-84-feat: 에약내역 페이지 로직 추가

--- src/app/(user)/reservations/page.tsx
+++ src/app/(user)/reservations/page.tsx
@@ -1,5 +1,23 @@
-function Reservations() {
-  return <div>예약 내역</div>;
+import myReservationAPI from "@/apis/myReservationAPI";
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+
+import MyReservations from "./_components/MyReservations";
+
+async function Reservations({ status = undefined }) {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["myReservations", { size: 10, status }],
+    queryFn: () => myReservationAPI.get({ cursorId: undefined, size: 10, status }),
+  });
+
+  return (
+    <div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <MyReservations initialStatus={status} />
+      </HydrationBoundary>
+    </div>
+  );
 }
 
 export default Reservations;
