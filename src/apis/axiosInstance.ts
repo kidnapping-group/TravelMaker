b6c75ab80afbd34e9410d48ef94d00312c271fd8
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-16 04:24:35 +0900

    ST-92-fix: 오류 수정

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 import Cookies from "js-cookie";
-import { redirect } from "next/navigation";
 
 import handleAxiosError from "./ApiError";
 
@@ -33,14 +32,16 @@ axiosInstance.interceptors.response.use(
     if (error.response.status === 401 && !prevRequest.retry) {
       prevRequest.retry = true;
 
-      let currentRefreshToken = Cookies.get("refreshToken");
+      const currentRefreshToken = Cookies.get("refreshToken");
       if (!currentRefreshToken) {
         return Promise.reject(error);
       }
-
       try {
-        axiosInstance.defaults.headers.common.Authorization = `Bearer ${currentRefreshToken}`;
-        const { data } = await axiosInstance.post("/auth/tokens");
+        const { data } = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/tokens`, null, {
+          headers: {
+            Authorization: `Bearer ${currentRefreshToken}`,
+          },
+        });
         const { accessToken, refreshToken } = data;
         Cookies.set("refreshToken", refreshToken);
         Cookies.set("accessToken", accessToken);
@@ -51,12 +52,11 @@ axiosInstance.interceptors.response.use(
         Cookies.remove("refreshToken");
         Cookies.remove("accessToken");
         Cookies.remove("social-login-store");
-        redirect("/signin?expiredRefreshToken=true");
+        window.location.href = "/signin?expiredRefreshToken=true";
       }
     }
     handleAxiosError(error);
     return Promise.reject(error);
   },
 );
-
 export default axiosInstance;
