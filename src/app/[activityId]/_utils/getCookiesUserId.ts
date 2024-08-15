c3e7d303561688b8566412867e20c97d7570e2dc
Darkthos <kiki7951@naver.com>
2024-08-15 21:52:16 +0900

    ST-25-feat: 쿠키 꺼내오는 함수 타입 추가

--- src/app/[activityId]/_utils/getCookiesUserId.ts
+++ src/app/[activityId]/_utils/getCookiesUserId.ts
@@ -1,9 +1,24 @@
 import Cookies from "js-cookie";
 
-const getCookiesUserID = () => {
+interface UserProfile {
+  social: boolean;
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface SocialLoginStore {
+  state: UserProfile;
+  version: number;
+}
+
+const getCookiesUserID = (): number | null => {
   const data = Cookies.get("social-login-store");
   if (data) {
-    const parseData = JSON.parse(data);
+    const parseData: SocialLoginStore = JSON.parse(data);
     return parseData.state.id;
   }
   return null;
