bb7f0ee94cd7e620ec931bd8cc01c9a6b3e431ae
Darkthos <kiki7951@naver.com>
2024-08-15 20:36:27 +0900

    ST-25-fix: 예약 시간 선택 성능 상향

--- src/app/[activityId]/_components/ReservationSteps/FreeReservationTime.tsx
+++ src/app/[activityId]/_components/ReservationSteps/FreeReservationTime.tsx
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
 function FreeReservationTime({ reservationTimes }: { reservationTimes: string[][] }) {
-  const [selectTime, setSelectTime] = useState(false);
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
+
+  const handleTimeSelection = (time: string) => {
+    setSelectedTime(time === selectedTime ? null : time);
+  };
 
   return (
     <div>
       <h2 className="mt-4">예약 가능한 시간</h2>
       <div className="mt-3 grid w-fit grid-cols-2 gap-3">
-        {reservationTimes.map(item => (
-          <button
-            key={crypto.randomUUID()}
-            type="button"
-            onClick={() => setSelectTime(true)}
-            className={`flex h-11 w-28 items-center justify-center rounded-lg border border-primary-400 text-lg font-medium text-primary-400 ${selectTime && "focus:bg-primary-400 focus:text-white"}`}
-          >
-            {item.join("~")}
-          </button>
-        ))}
+        {reservationTimes.map(item => {
+          const timeString = item.join("~");
+          const isSelected = selectedTime === timeString;
+          return (
+            <button
+              key={crypto.randomUUID()}
+              type="button"
+              onClick={() => handleTimeSelection(timeString)}
+              className={`flex h-11 w-28 items-center justify-center rounded-lg border border-primary-400 text-lg font-medium ${
+                isSelected ? "bg-primary-400 text-white" : "text-primary-400"
+              }`}
+            >
+              {timeString}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
