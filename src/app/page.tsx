d8b1cdcb29e811e51ccbe205e7a733d4766b58f0
Seungheon Lee <36994104+heony704@users.noreply.github.com>
2024-08-16 18:04:14 +0900

    feat: 메인 페이지 체험 리스트 구현 (ST-70) (#65)
    * ST-70-feat: 체험 카드 컴포넌트 추가
    
    * ST-70-feat: 체험 섹션들 추가
    
    * ST-70-style: 메인 페이지 양쪽 여백 수정
    
    * ST-70-feat: 메인 페이지 체험 카드 별 아이콘 교체
    
    * ST-70-style: 메인페이지 양 옆 여백 수정
    
    * ST-70-refactor: 체험카드 이름 수정
    
    * ST-70-refactor: 컴포넌트 props 타입 이름 수정
    
    ---------
    
    Co-authored-by: MountionRiver <162934516+JooKangsan@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ActivitySection from "@/app/_components/ActivitySection";
 import BigActivitySection from "@/app/_components/BigActivitySection";
 import SearchForm from "@/app/_components/SearchForm";
 
@@ -23,6 +24,16 @@ async function Home() {
       <div className="flex w-full justify-center px-5 pc:px-10">
         <div className="mb-[100px] mt-[50px] flex w-full max-w-[1200px] flex-col gap-5">
           <BigActivitySection title="실시간 인기 체험" sort="most_reviewed" />
+          <ActivitySection title="새로 오픈한 체험" sort="latest" />
+          <ActivitySection title="일상을 풍요롭게 만드는 특별한 경험 🎨🎶" category="arts" />
+          <ActivitySection title="입맛을 사로잡는 미식 여행 🍽️" category="food" />
+          <ActivitySection title="에너지 넘치는 스포츠 체험 ⚽" category="sports" />
+          <ActivitySection
+            title="발걸음을 따라가는 새로운 이야기, 특별한 투어 🚶‍♂️"
+            category="tour"
+          />
+          <ActivitySection title="웰빙으로 건강한 일상 만들기 🌱" category="wellbeing" />
+          <ActivitySection title="손끝에 전해지는 짜릿한 손맛! 🐟" keyword="낚시" />
         </div>
       </div>
     </main>
