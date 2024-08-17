8598eb5bcb303d4dc3caf3eb4b1c7660f7cc85e2
heony704 <heony704@gmail.com>
2024-08-17 15:22:15 +0900

    ST-68-feat: 체험 섹션 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,10 +24,7 @@ async function Home() {
           <ActivitySection title="일상을 풍요롭게 만드는 특별한 경험 🎨🎶" category="arts" />
           <ActivitySection title="입맛을 사로잡는 미식 여행 🍽️" category="food" />
           <ActivitySection title="에너지 넘치는 스포츠 체험 ⚽" category="sports" />
-          <ActivitySection
-            title="발걸음을 따라가는 새로운 이야기, 특별한 투어 🚶‍♂️"
-            category="tour"
-          />
+          <ActivitySection title="일상 밖의 모험, 숨은 명소로의 초대장 💌" category="tour" />
           <ActivitySection title="웰빙으로 건강한 일상 만들기 🌱" category="wellbeing" />
           <ActivitySection title="손끝에 전해지는 짜릿한 손맛! 🐟" keyword="낚시" />
         </div>
