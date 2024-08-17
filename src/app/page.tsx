ab22372d9800cf6eb5658d073ed7fe4bb55f9f25
heony704 <heony704@gmail.com>
2024-08-17 10:27:58 +0900

    ST-68-feat: 메인 페이지 배너 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import activitiesAPI from "@/apis/activitiesAPI";
+import Banner from "@/app/_components/Banner";
 import SearchForm from "@/app/_components/SearchForm";
 import Link from "next/link";
 
@@ -7,21 +8,17 @@ async function Home() {
 
   return (
     <main className="flex flex-col items-center">
-      <section className="flex w-full justify-center bg-black bg-opacity-50">
-        <div className="mx-5 flex w-full max-w-[1200px] flex-col gap-8 pb-[50px] pt-[100px]">
-          <div className="font-bold text-white">
-            <h2 className="text-3xl">
-              함께 배우면 즐거운
-              <br /> 스트릿 댄스
-            </h2>
-            <p className="text-xl">1월의 인기 체험 BEST 🔥</p>
-          </div>
-          <div className="flex flex-col gap-5 rounded-2xl bg-white p-5">
+      <div className="relative flex w-full">
+        <Banner />
+
+        <div className="absolute bottom-16 flex w-full justify-center px-5 pc:px-10">
+          <div className="flex w-full max-w-[1200px] flex-col gap-5 rounded-2xl bg-white p-5">
             <h2 className="text-2lg font-bold">무엇을 체험하고 싶으신가요?</h2>
             <SearchForm placeholder="내가 원하는 체험은" />
           </div>
         </div>
-      </section>
+      </div>
+
       <div className="mb-[100px] mt-[50px] flex w-full max-w-[1200px] flex-col">
         <section className="flex flex-col">
           <h2 className="text-2lg font-bold text-black">🔥 인기 체험</h2>
