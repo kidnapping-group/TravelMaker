87aca0179620f0718b5e32538908025a603abf09
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-08-19 13:01:06 +0900

    feat: 유저상태의 헤더 ui 구현 ST-97 (#73)
    * ST-97-feat: 유저 있을 시의 헤더 ui 구현
    
    * ST-97-style: css수정
    
    * ST-97-refactor: HeaderLayout, DropdownToggle이름 변경
    
    * ST-97-fix: ui깜빡꺼림 없이 완벽하게 작동
    
    * ST-97-refact: css수정, 무쓸모 코드 제거

---
+++ src/hooks/useDropdownToggle.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+const useDropdownToggle = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  return { isOpen, toggleDropdown, dropdownRef };
+};
+
+export default useDropdownToggle;
