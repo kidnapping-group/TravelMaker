c90916c4bb33cfa0af2e93bf2bebe5b57fd95e84
heony704 <heony704@gmail.com>
2024-08-20 07:45:43 +0900

    ST-67-fix: 드롭다운 placeholder에 현재 sort 표시

--- src/app/search/_components/SortDropdown.tsx
+++ src/app/search/_components/SortDropdown.tsx
@@ -2,6 +2,7 @@
 
 import Dropdown from "@/components/Dropdown";
 import useUpdateQuery from "@/hooks/useUpdateQuery";
+import { useSearchParams } from "next/navigation";
 
 const sortTitle: Record<string, string> = {
   인기순: "most_reviewed",
@@ -13,6 +14,11 @@ const sortTitle: Record<string, string> = {
 function SortDropdown() {
   const updateQuery = useUpdateQuery();
 
+  const searchParams = useSearchParams();
+  const currentSort = searchParams.get("sort");
+
+  const currentSortName = Object.keys(sortTitle).find(key => sortTitle[key] === currentSort);
+
   const handleMenuItemClick = (status: string) => {
     updateQuery("sort", sortTitle[status]);
   };
@@ -20,6 +26,7 @@ function SortDropdown() {
   return (
     <Dropdown
       type="round"
+      placeHolder={currentSortName}
       menuItems={["인기순", "최신순", "낮은 가격순", "높은 가격순"]}
       onChangeDropdown={handleMenuItemClick}
     />
