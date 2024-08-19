bf1e23c2ce113f54d3557c287ccc70623f60d8d7
heony704 <heony704@gmail.com>
2024-08-20 07:55:26 +0900

    ST-67-refactor: 정렬 객체 수정 & 정적 데이터 상수화

--- src/app/search/_components/SortDropdown.tsx
+++ src/app/search/_components/SortDropdown.tsx
@@ -4,30 +4,32 @@ import Dropdown from "@/components/Dropdown";
 import useUpdateQuery from "@/hooks/useUpdateQuery";
 import { useSearchParams } from "next/navigation";
 
-const sortTitle: Record<string, string> = {
-  인기순: "most_reviewed",
-  최신순: "latest",
-  "낮은 가격순": "price_asc",
-  "높은 가격순": "price_desc",
+const SORT_NAME: Record<string, string> = {
+  most_reviewed: "인기순",
+  latest: "최신순",
+  price_asc: "낮은 가격순",
+  price_desc: "높은 가격순",
 };
+const PARAM_NAME = "sort";
 
 function SortDropdown() {
   const updateQuery = useUpdateQuery();
 
   const searchParams = useSearchParams();
-  const currentSort = searchParams.get("sort");
-
-  const currentSortName = Object.keys(sortTitle).find(key => sortTitle[key] === currentSort);
+  const currentSort = searchParams.get(PARAM_NAME);
 
   const handleMenuItemClick = (status: string) => {
-    updateQuery("sort", sortTitle[status]);
+    const selectedSortKey = Object.keys(SORT_NAME).find(key => SORT_NAME[key] === status);
+    if (selectedSortKey) {
+      updateQuery(PARAM_NAME, selectedSortKey);
+    }
   };
 
   return (
     <Dropdown
       type="round"
-      placeHolder={currentSortName}
-      menuItems={["인기순", "최신순", "낮은 가격순", "높은 가격순"]}
+      placeHolder={currentSort ? SORT_NAME[currentSort] : undefined}
+      menuItems={Object.values(SORT_NAME)}
       onChangeDropdown={handleMenuItemClick}
     />
   );
