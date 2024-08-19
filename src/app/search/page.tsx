cf9c380eabf74eb2003f483b7224d4dbfe4adf97
heony704 <heony704@gmail.com>
2024-08-20 06:45:18 +0900

    ST-67-style: 검색 페이지 스타일 수정
    - div 태그 사용 최소화
    - section 태그 사용
    - 스타일 (여백) 수정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -25,27 +25,27 @@ async function SearchPage({ searchParams }: SearchPageProps) {
   });
 
   return (
-    <main className="flex flex-col items-center px-5">
-      <div className="flex w-full max-w-[1200px] flex-col">
-        <div className="py-5">
-          <SearchForm placeholder="내가 원하는 체험은" />
+    <main className="flex flex-col items-center px-5 pc:px-10">
+      <section className="w-full max-w-[1200px] py-5">
+        <SearchForm placeholder="내가 원하는 체험은" />
+      </section>
+
+      <section className="flex w-full max-w-[1200px] flex-col gap-3 pb-[100px]">
+        <h2 className="text-xl font-bold">
+          &lsquo;{searchParams.keyword}&rsquo; 검색 결과 {totalCount}개
+        </h2>
+
+        <div className="flex items-center justify-between">
+          <TabList
+            paramName="category"
+            paramValues={["문화 · 예술", "식음료", "스포츠", "투어", "관광", "웰빙"]}
+          />
+          <SortDropdown />
         </div>
 
-        <div className="flex w-full flex-col gap-2">
-          <h2 className="text-xl font-bold">
-            &lsquo;{searchParams.keyword}&rsquo; 검색 결과 {totalCount}개
-          </h2>
-
-          <div className="flex items-center justify-between">
-            <TabList
-              paramName="category"
-              paramValues={["문화 · 예술", "식음료", "스포츠", "투어", "관광", "웰빙"]}
-            />
-            <SortDropdown />
-          </div>
-
-          {activities.length > 0 ? (
-            <div className="-ml-3 mt-3 flex flex-wrap justify-center tablet:-ml-5">
+        {activities.length > 0 ? (
+          <div className="flex flex-col gap-5 pt-3">
+            <div className="-ml-3 flex flex-wrap tablet:-ml-5">
               {activities.map(({ id, bannerImageUrl, title, price, rating, reviewCount }) => (
                 <div
                   className="mb-3 basis-1/2 pl-3 tablet:mb-5 tablet:basis-1/3 tablet:pl-5 pc:basis-1/4"
@@ -62,15 +62,15 @@ async function SearchPage({ searchParams }: SearchPageProps) {
                   </Link>
                 </div>
               ))}
-              <Pagination totalCount={totalCount} pageSize={pageSize} />
             </div>
-          ) : (
-            <div className="my-20 flex justify-center text-lg font-medium text-gray-400">
-              검색 결과가 없어요.
-            </div>
-          )}
-        </div>
-      </div>
+            <Pagination totalCount={totalCount} pageSize={pageSize} />
+          </div>
+        ) : (
+          <div className="my-20 flex justify-center text-lg font-medium text-gray-400">
+            검색 결과가 없어요.
+          </div>
+        )}
+      </section>
     </main>
   );
 }
