8cb9db22239d0e6a51df9e989eb0f681c02ac0c1
heony704 <heony704@gmail.com>
2024-08-20 15:18:07 +0900

    ST-114-feat: 테스트 페이지 추가

---
+++ src/app/test/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Dropdown from "@/components/Dropdown";
+
+function page() {
+  return (
+    <main className="mx-20 my-20 flex flex-col gap-10">
+      <Dropdown
+        menuItems={["인기순", "가격 낮은순", "가격 높은순", "최신순"]}
+        type="round"
+        onChangeDropdown={() => {}}
+      />
+      <Dropdown
+        menuItems={["인기순", "가격 낮은순", "가격 높은순", "최신순"]}
+        type="square"
+        onChangeDropdown={() => {}}
+      />
+    </main>
+  );
+}
+
+export default page;
