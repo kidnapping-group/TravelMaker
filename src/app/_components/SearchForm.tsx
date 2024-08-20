4ce4857c30140e3f4d87520f6ca54e7450ba7107
heony704 <heony704@gmail.com>
2024-08-20 17:18:24 +0900

    ST-116-feat: 검색창에 원래 검색어 표시

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import { Button } from "@/components/Button";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 interface Props {
   placeholder: string;
 }
 
 function SearchForm({ placeholder }: Props) {
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword");
+
   return (
     <form className="flex gap-3" action="/search" method="get">
       <div className="relative flex grow items-center">
@@ -21,6 +27,7 @@ function SearchForm({ placeholder }: Props) {
           className="h-full w-full rounded-lg bg-gray-100 py-2 pl-12 pr-4 text-lg font-medium placeholder:text-gray-400"
           name="keyword"
           placeholder={placeholder}
+          defaultValue={keyword ?? undefined}
         />
       </div>
       <Button size="large" type="submit">
