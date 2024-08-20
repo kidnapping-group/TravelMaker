1d211b4ddf8f7f6318fd712fa9314aa6e7b792c4
Darkthos <kiki7951@naver.com>
2024-08-20 14:50:24 +0900

    ST-31-feat: 리액트 쿼리 추가

--- src/app/(user)/reservation-status/page.tsx
+++ src/app/(user)/reservation-status/page.tsx
@@ -1,5 +1,22 @@
-function ReservationStatus() {
-  return <div>예약 현황</div>;
+import myReservationAPI from "@/apis/myReservationAPI";
+import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
+import Image from "next/image";
+
+import Bed from "../../../../../icons/bed.svg";
+
+async function ReservationStatusPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["myReservations"],
+    queryFn: () => myReservationAPI.get({ cursorId: undefined, size: 1000 }),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Image src={Bed} width={3} height={3} />
+      <h1>예약 현황</h1>
+    </HydrationBoundary>
+  );
 }
 
-export default ReservationStatus;
+export default ReservationStatusPage;
