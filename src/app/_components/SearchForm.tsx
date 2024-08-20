628efa54fa485e716147894afb96486b4371d6a6
heony704 <heony704@gmail.com>
2024-08-21 04:22:14 +0900

    ST-116-feat: input 값이 없을 때 검색 방지 & tooltip 표시

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { Button } from "@/components/Button";
+import Tooltip from "@/components/Tooltip";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
   placeholder: string;
 }
 
 function SearchForm({ placeholder }: Props) {
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
   const { push } = useRouter();
 
   const searchParams = useSearchParams();
@@ -20,14 +24,19 @@ function SearchForm({ placeholder }: Props) {
     const formData = new FormData(event.currentTarget);
     const searchKeyword = formData.get("keyword") as string;
 
+    if (!searchKeyword.trim()) {
+      setIsTooltipOpen(true);
+      return;
+    }
+
     push(`/search?keyword=${searchKeyword}`);
   };
 
   return (
     <form className="flex gap-3" onSubmit={handleSearch}>
-      <div className="relative flex grow items-center">
+      <div className="relative flex grow items-center justify-center">
         <Image
-          className="absolute"
+          className="absolute left-0"
           src="/icons/bed.svg"
           width={48}
           height={48}
@@ -39,6 +48,11 @@ function SearchForm({ placeholder }: Props) {
           name="keyword"
           placeholder={placeholder}
           defaultValue={keyword}
+        />
+        <Tooltip
+          isOpen={isTooltipOpen}
+          close={() => setIsTooltipOpen(false)}
+          text="체험을 입력해주세요."
         />
       </div>
       <Button size="large" type="submit">
