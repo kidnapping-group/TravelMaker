4ca8613f3141a449adbc2a7323c3a8e2cd0f7819
JustDevRae <ksrae165@gmail.com>
2024-08-21 18:06:34 +0900

    ST-27-fix: schema optional 속성 제거

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -5,15 +5,11 @@ const baseSchema = z.object({
     .string()
     .min(2, "이름을 입력해주세요")
     .max(13, "13자 이하로 작성해주세요")
-    .regex(/^[^!@#$%^&*(),.?":{}|<>]*$/, "이름에 특수문자가 포함될 수 없습니다")
-    .optional(),
+    .regex(/^[^!@#$%^&*(),.?":{}|<>]*$/, "이름에 특수문자가 포함될 수 없습니다"),
   email: z.string().min(1, "이메일을 입력해주세요").email("이메일 형식으로 작성해 주세요"),
-  password: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요").optional(),
-  confirmPassword: z
-    .string()
-    .min(1, "비밀번호를 입력해주세요")
-    .min(8, "8자 이상 입력해주세요")
-    .optional(),
+  password: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요"),
+  confirmPassword: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요"),
+
   price: z.preprocess(
     value => {
       const parsedValue = Number(value);
@@ -25,7 +21,7 @@ const baseSchema = z.object({
     z.number().min(0, "0이상의 숫자만 입력해주세요"),
   ),
   textarea: z.string().max(500, "500자 이하로 작성해주세요"),
-  profileImageUrl: z.any().optional(),
+  profileImageUrl: z.any(),
 });
 
 export default baseSchema;
