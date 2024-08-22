5189a48a058ac4c4bd6279fb1c3202b4298a54c6
Seungheon Lee <36994104+heony704@users.noreply.github.com>
2024-08-22 15:22:29 +0900

    feat: 검색창에 검색 내용 표시 & 입력값 없을 때 검색 방지 (ST-116) (#93)
    * ST-116-feat: 검색창에 원래 검색어 표시
    
    * ST-116-refactor: 검색 로직 client적으로 수정
    
    - 페이지를 새로고침하지 않고 Next 라우팅 적용
    
    * ST-116-chore: tailwind transparent color 추가
    
    * ST-116-feat: input 값이 없을 때 검색 방지 & tooltip 표시
    
    * ST-116-feat: Props 타입 이름 수정
    
    * ST-116-feat: 인코딩 처리

--- src/app/_components/SearchForm.tsx
+++ src/app/_components/SearchForm.tsx
@@ -1,16 +1,43 @@
-import { Button } from "@/components/Button";
-import Image from "next/image";
+"use client";
 
-interface Props {
+import { Button } from "@/components/Button";
+import Tooltip from "@/components/Tooltip";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+interface SearchFormProps {
   placeholder: string;
 }
 
-function SearchForm({ placeholder }: Props) {
+function SearchForm({ placeholder }: SearchFormProps) {
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
+  const { push } = useRouter();
+
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword")?.toString();
+
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const searchKeyword = formData.get("keyword") as string;
+
+    if (!searchKeyword.trim()) {
+      setIsTooltipOpen(true);
+      return;
+    }
+
+    const encodedKeyword = encodeURIComponent(searchKeyword);
+    push(`/search?keyword=${encodedKeyword}`);
+  };
+
   return (
-    <form className="flex gap-3" action="/search" method="get">
-      <div className="relative flex grow items-center">
+    <form className="flex gap-3" onSubmit={handleSearch}>
+      <div className="relative flex grow items-center justify-center">
         <Image
-          className="absolute"
+          className="absolute left-0"
           src="/icons/bed.svg"
           width={48}
           height={48}
@@ -21,6 +48,12 @@ function SearchForm({ placeholder }: Props) {
           className="h-full w-full rounded-lg bg-gray-100 py-2 pl-12 pr-4 text-lg font-medium placeholder:text-gray-400"
           name="keyword"
           placeholder={placeholder}
+          defaultValue={keyword}
+        />
+        <Tooltip
+          isOpen={isTooltipOpen}
+          close={() => setIsTooltipOpen(false)}
+          text="체험을 입력해주세요."
         />
       </div>
       <Button size="large" type="submit">
