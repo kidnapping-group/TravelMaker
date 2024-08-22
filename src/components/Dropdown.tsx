b98b33022945bfb5685ed327081e410758752568
heony704 <heony704@gmail.com>
2024-08-22 11:35:11 +0900

    ST-114-feat: Dropdown 스타일 하나로 통일

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,41 +1,20 @@
 "use client";
 
 import Image from "next/image";
-import { FocusEvent, KeyboardEvent, useCallback, useMemo, useRef, useState } from "react";
+import { FocusEvent, KeyboardEvent, useCallback, useRef, useState } from "react";
 
 interface DropdownProps {
   menuItems: string[];
-  type: "square" | "round";
   onChangeDropdown: (status: string) => Promise<void> | void;
   placeHolder?: string;
 }
 
-const styleConfig = {
-  round: {
-    container: "w-auto text-lg font-medium",
-    button: "rounded-2xl text-green border-green p-4",
-    item: "border-b text-center py-4 h-14 text-[#4B4B4B]",
-    selectedText: "text-green",
-    dropdownList: "",
-    image: { style: "dropdown-small", size: 16 },
-  },
-  square: {
-    container: "w-full text-base font-light",
-    button: "rounded-[4px] text-[#A1A1A1] border-gray-500 py-4 pl-4",
-    item: "text-left pl-2 py-2 h-10",
-    selectedText: "text-gray-600",
-    dropdownList: "p-2",
-    image: { style: "dropdown", size: 48 },
-  },
-};
-
-function Dropdown({ menuItems, type = "round", onChangeDropdown, placeHolder }: DropdownProps) {
+function Dropdown({ menuItems, onChangeDropdown, placeHolder }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(placeHolder || menuItems[0]);
   const [focusedIndex, setFocusedIndex] = useState(-1);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const itemRefs = useRef<(HTMLButtonElement | null)[]>([]);
-  const styles = useMemo(() => styleConfig[type], [type]);
 
   const handleItemClick = useCallback(
     async (item: string) => {
@@ -81,50 +60,46 @@ function Dropdown({ menuItems, type = "round", onChangeDropdown, placeHolder }: 
   }, []);
 
   return (
-    <div>
-      <div
-        role="button"
-        className={`relative inline-block min-w-32 bg-white ${styles.container}`}
-        ref={dropdownRef}
-        onBlur={handleBlur}
-        onKeyDown={handleKeyDown}
-        tabIndex={0}
+    <div
+      role="button"
+      className="relative w-32"
+      ref={dropdownRef}
+      onBlur={handleBlur}
+      onKeyDown={handleKeyDown}
+      tabIndex={0}
+    >
+      <button
+        type="button"
+        className="flex h-11 w-full items-center rounded-lg bg-gray-100 text-left text-md font-medium text-black outline-none transition-colors hover:bg-gray-200 active:bg-gray-300"
+        onClick={toggleDropdown}
       >
-        <button
-          type="button"
-          className={`flex h-14 w-full items-center justify-between gap-2 whitespace-nowrap border ${styles.button} text-left shadow-sm outline-none`}
-          onClick={toggleDropdown}
-        >
-          <span className={styles.selectedText}>{selectedItem}</span>
-          <Image
-            src={`/icons/icon-${isOpen ? "dropup" : styles.image.style}.svg`}
-            alt="드롭다운 버튼"
-            width={styles.image.size}
-            height={styles.image.size}
-          />
-        </button>
-        {isOpen && (
-          <div
-            className={`absolute z-10 ${styles.dropdownList} mt-2 w-full rounded-md border border-[#1122110D] bg-white shadow-sm`}
-          >
-            {menuItems.map((item, index) => (
-              <button
-                className={`block w-full ${styles.item} rounded-md hover:bg-gray-200 ${
-                  index === focusedIndex ? "bg-gray-200" : ""
-                }`}
-                type="button"
-                key={item}
-                ref={(el: HTMLButtonElement | null) => {
-                  itemRefs.current[index] = el;
-                }}
-                onClick={() => handleItemClick(item)}
-              >
-                {item}
-              </button>
-            ))}
-          </div>
-        )}
-      </div>
+        <span className="grow truncate pl-[18px]">{selectedItem}</span>
+        <Image
+          className="mr-1"
+          src={`/icons/icon-${isOpen ? "dropup" : "dropdown"}.svg`}
+          alt="드롭다운 버튼"
+          width={32}
+          height={32}
+        />
+      </button>
+      {isOpen && (
+        <div className="absolute z-10 mt-2 flex max-h-[300px] w-full flex-col overflow-y-auto rounded-lg bg-white p-[6px] shadow-[0_6px_20px_rgba(0,0,0,0.15)]">
+          {menuItems.map((item, index) => (
+            <button
+              className={`${index === focusedIndex ? "bg-gray-100 text-primary-500" : ""} h-11 shrink-0 rounded-lg px-3 text-left text-md hover:bg-gray-100 hover:text-primary-500 focus:bg-gray-100 active:bg-gray-200`}
+              type="button"
+              key={item}
+              tabIndex={0}
+              ref={(el: HTMLButtonElement | null) => {
+                itemRefs.current[index] = el;
+              }}
+              onClick={() => handleItemClick(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
