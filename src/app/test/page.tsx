8800d1baa5c0ec5701a7ea241e2d2c2738ce2380
heony704 <heony704@gmail.com>
2024-08-22 16:23:19 +0900

    ST-114-feat: test용 긴 dropdown

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,13 +6,21 @@ function page() {
   return (
     <main className="mx-20 my-20 flex flex-col gap-10">
       <Dropdown
-        menuItems={["인기순", "가격 낮은순", "가격 높은순", "최신순"]}
-        type="round"
+        menuItems={[
+          "인기순",
+          "가격 낮은순",
+          "가격 높은순",
+          "최신순",
+          "최신순",
+          "최신순",
+          "최신순",
+          "최신순",
+          "최신순",
+        ]}
         onChangeDropdown={() => {}}
       />
       <Dropdown
         menuItems={["인기순", "가격 낮은순", "가격 높은순", "최신순"]}
-        type="square"
         onChangeDropdown={() => {}}
       />
     </main>
