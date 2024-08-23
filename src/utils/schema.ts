a77928e75b6b81829344b88f914ac4c4d5be7c7d
JustDevRae <ksrae165@gmail.com>
2024-08-23 17:45:59 +0900

    ST-27-refactor: 닉네임 스키마 글자 수 10자로 제한

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -4,7 +4,7 @@ const baseSchema = z.object({
   nickname: z
     .string()
     .min(2, "이름을 입력해주세요")
-    .max(13, "13자 이하로 작성해주세요")
+    .max(10, "10자 이하로 작성해주세요")
     .regex(/^[^!@#$%^&*(),.?":{}|<>]*$/, "이름에 특수문자가 포함될 수 없습니다"),
   email: z.string().min(1, "이메일을 입력해주세요").email("이메일 형식으로 작성해 주세요"),
   password: z.string().min(1, "비밀번호를 입력해주세요").min(8, "8자 이상 입력해주세요"),
