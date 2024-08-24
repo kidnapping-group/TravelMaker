33e5479f0b9ae007888dbf735b8576b94cfbed9e
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-24 17:51:12 +0900

    ST-117-feat: 에러 페이지 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import { APIError, getErrorStatusMessage } from "@/apis/ApiError";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import notFound from "../../public/images/404.png";
 
-export default function Error({ error }: { error: Error }) {
+interface ErrorProps {
+  error: Error | APIError;
+}
+
+export default function Error({ error }: ErrorProps) {
   const router = useRouter();
+  const isAPIError = error instanceof APIError;
+  const statusCode = isAPIError ? (error as APIError).status : null;
 
   return (
-    <div className="fixed inset-0 flex flex-col items-center justify-center tablet:flex-row">
+    <div className="inset-0 flex h-[100vh] flex-col items-center justify-center gap-4 px-10 tablet:flex-row">
       <Image
         src={notFound}
         alt="Error"
@@ -17,12 +24,18 @@ export default function Error({ error }: { error: Error }) {
         height={240}
         className="animate-[spin_1s_linear_infinite]"
       />
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-medium">
-          {error.message.slice(0, 3)} Error: <span>{error.message.slice(3)}</span>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h1 className="text-3xl font-bold tablet:text-[60px]">
+          {statusCode} {statusCode && getErrorStatusMessage(statusCode)}
         </h1>
-        {error.message.slice(0, 3) === "404" && <p>페이지를 찾을 수가 없습니다.</p>}
-        <button type="button" onClick={() => router.back()} className="text-left text-primary-500">
+        <h1 className="text-2xl font-medium">
+          <span>{error.message}</span>
+        </h1>
+        <button
+          type="button"
+          onClick={() => router.back()}
+          className="text-left text-xl text-primary-500"
+        >
           이전 페이지로 돌아가기
         </button>
       </div>
