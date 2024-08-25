2e07cc5b97b391e46b6d03cdd13f148f0f75a26e
JooKangsan <162934516+JooKangsan@users.noreply.github.com>
2024-08-26 02:46:34 +0900

    ST-138-fix: auth layout 파일 삭제

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,20 +1,11 @@
-import { cookies, headers } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 function AuthRootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const accessToken = cookies().get("accessToken");
-
-  if (accessToken) {
-    const referer = headers().get("referer") || "/";
-    redirect(referer);
-  }
-
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_URL}/social/kakao&scope=profile_nickname,profile_image`;
   const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20email&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_URL}/social/google`;
 
