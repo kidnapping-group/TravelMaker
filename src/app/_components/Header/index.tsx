e2a4960e389ec4adff645f6b0bd7af67bd26eda0
Darkthos <kiki7951@naver.com>
2024-08-26 16:04:12 +0900

    ST-136-merge: main

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -3,31 +3,13 @@
 import UserExist from "@/app/_components/Header/UserExist";
 import UserNotExist from "@/app/_components/Header/UserNotExist";
 import socialLoginStore from "@/store/socialLoginStore";
-import logoutCookies from "@/utils/logoutCookies";
-import { useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import Link from "next/link";
 
 function Header() {
-  const router = useRouter();
-  const queryClient = useQueryClient();
-  const logout = socialLoginStore(state => state.logout);
-  const userInfo = socialLoginStore(state => ({
+  const { id } = socialLoginStore(state => ({
     id: state.id,
-    email: state.email,
-    nickname: state.nickname,
-    profileImageUrl: state.profileImageUrl,
-    createdAt: state.createdAt,
-    updatedAt: state.updatedAt,
-    social: state.social,
   }));
-
-  const handleLogout = () => {
-    logout();
-    logoutCookies();
-    router.push("/");
-    queryClient.removeQueries();
-  };
 
   return (
     <header className="sticky top-0 z-20 border-b border-gray-200 bg-white">
@@ -35,7 +17,7 @@ function Header() {
         <Link href="/">
           <Image src="/images/logo_small.png" alt="헤더 로고" width={165} height={55} priority />
         </Link>
-        {userInfo?.id ? <UserExist /> : <UserNotExist />}
+        {id ? <UserExist /> : <UserNotExist />}
       </nav>
     </header>
   );
