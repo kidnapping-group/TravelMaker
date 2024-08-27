b704159eb8b5ea2933a3db8e4a9aa76e7d3545a8
heony704 <heony704@gmail.com>
2024-08-27 13:14:51 +0900

    ST-147-style: 메인 페이지 체험 섹션 간 여백 확장

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,7 +20,7 @@ function Home() {
         </div>
 
         <div className="flex w-full justify-center px-5 pc:px-10">
-          <div className="mb-[100px] mt-[50px] flex w-full max-w-[1200px] flex-col gap-5">
+          <div className="mb-[100px] mt-[50px] flex w-full max-w-[1200px] flex-col gap-10">
             <BigActivitySection title="실시간 인기 체험" sort="most_reviewed" />
             <ActivitySection title="새로 오픈한 체험" sort="latest" />
             <ActivitySection title="일상을 풍요롭게 만드는 특별한 경험 🎨🎶" category="arts" />
