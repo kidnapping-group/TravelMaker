3f6f4e1cec304531463e56940ebab7b3c1a9eee3
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-08-28 21:47:53 +0900

    refactor: main page 리액트 쿼리적용(체험 삭제 시, 즉각 반영되게), 모든 페이지 error이미지 처리, 체험관리 예약내역 팝업 수정(ST-163) (#135)
    * ST-163-refactor: 메인 페이지 쿼리 키, 모든 페이지 엑박 이미지 대처
    
    * ST-163-refactor: 체험 관리에서 예약 popup텍스트 뜨는 거수정 버튼 좌우 수정

--- src/app/(home)/_components/ActivityCard.tsx
+++ src/app/(home)/_components/ActivityCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import useImageError from "@/hooks/useImageError";
 import Image from "next/image";
 import { FaStar } from "react-icons/fa6";
 
@@ -18,12 +21,20 @@ function ActivityCard({
   rating,
   reviewCount,
 }: ActivityCardProps) {
+  const [errorImage] = useImageError(["/images/noImage.png"]);
+
   return (
     <article
       className={`${wide ? "w-full" : "w-[288px] pc:w-full"} relative flex flex-col gap-4 pb-2`}
     >
       <div className="group relative aspect-video w-full overflow-hidden rounded-[10px]">
-        <Image className="object-cover" src={bannerImageUrl} alt="체험 사진" fill />
+        <Image
+          className="object-cover"
+          onError={errorImage.onError}
+          src={errorImage.src || bannerImageUrl}
+          alt="체험 사진"
+          fill
+        />
         <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-20" />
       </div>
       <div className="mx-1 flex flex-col gap-1">
