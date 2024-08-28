1fa4c108769ee8334fb1664600a0ff567eebb965
heony704 <heony704@gmail.com>
2024-08-29 01:29:50 +0900

    ST-171-style: 메인 페이지 스타일 깨짐 수정

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -21,7 +21,7 @@ function Header() {
   }, [id]);
 
   return (
-    <header className="fixed top-0 z-20 flex h-[60px] w-full items-center border-b border-gray-200 bg-white">
+    <header className="fixed top-0 z-30 flex h-[60px] w-full items-center border-b border-gray-200 bg-white">
       <div className="mx-auto flex w-full items-center justify-between px-5 tablet:px-10 pc:max-w-[1200px] pc:px-0">
         <Link href="/">
           <Image src="/images/logo_small.png" alt="헤더 로고" width={165} height={55} priority />
