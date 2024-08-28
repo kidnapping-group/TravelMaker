5a3aa3c057bac56c63649cbfab293076eee1e8d9
Darkthos <kiki7951@naver.com>
2024-08-28 19:28:31 +0900

    ST-162-refactor: 후기 별점 소수점 첫번째자리까지 뜨게함

--- src/app/(home)/[activityId]/_components/Review/ReviewDetail.tsx
+++ src/app/(home)/[activityId]/_components/Review/ReviewDetail.tsx
@@ -2,14 +2,14 @@ import useGetCommentReviewViewModel from "@/app/(home)/[activityId]/_hooks/useGe
 import { FaStar } from "react-icons/fa6";
 
 function ReviewDetail() {
-  const { satisfies, totalCount, floorRating } = useGetCommentReviewViewModel();
+  const { satisfies, totalCount, rating } = useGetCommentReviewViewModel();
 
   return (
     <div>
       <div className="border-#112211 mt-10 border-t" />
       <h1 className="mt-10 text-xl font-bold">후기</h1>
       <div className="mt-4 flex items-center gap-4">
-        <p className="text-[50px] font-semibold">{floorRating}</p>
+        <p className="text-[50px] font-semibold">{rating}</p>
         <div className="flex flex-col justify-center gap-2">
           <p className="text-2lg font-normal">{satisfies}</p>
           <div className="flex items-center gap-2">
