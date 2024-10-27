939b86fdddf436df9edaebba1f40e3289ac67d5b
MountionRiver <you04042@gmail.com>
2024-10-27 14:15:49 +0900

    fix: 리뷰 작성후 리렌더링 안되는 문제 해결(ST-183) (#153)
    * ST-183-fix: 리뷰 작성후 리렌더링 안되는 문제 해결
    
    * ST-183-fix: 리뷰 모달 하단 padding값 추가

--- src/app/(user)/reservations/_components/review/Review.tsx
+++ src/app/(user)/reservations/_components/review/Review.tsx
@@ -5,18 +5,28 @@ import { Reservations } from "@/apis/API.type";
 import myReservationAPI from "@/apis/myReservationAPI";
 
 import { Button } from "@/components/Button";
+import { closeModal } from "@/components/Modal";
 
 import Rating from "./Rating";
 
-function Review({ reservation }: { reservation: Reservations }) {
+function Review({
+  reservation,
+  setReservationState,
+}: {
+  reservation: Reservations;
+  setReservationState: (updateFn: (prev: Reservations) => Reservations) => void;
+}) {
   const [reviewContent, setReviewContent] = useState<string>("");
   const [rating, setRating] = useState<number>(1);
 
   const handleReviewSubmit = async () => {
     await myReservationAPI.postReviews(reservation.id, { rating, content: reviewContent });
+    closeModal(`writeReview-${reservation.id}`);
+    setReservationState((prev: Reservations) => ({ ...prev, reviewSubmitted: true }));
   };
+
   return (
-    <div className="flex h-full w-[100vw] flex-col gap-3 px-4 tablet:w-[460px] tablet:gap-6 tablet:px-6">
+    <div className="tablet:h- flex h-full w-[100vw] flex-col gap-3 px-4 tablet:w-[460px] tablet:gap-6 tablet:px-6 tablet:pb-6">
       <div className="flex h-[100px] items-center gap-2 tablet:h-[137px]">
         <Image
           src={reservation.activity.bannerImageUrl}
@@ -30,19 +40,19 @@ function Review({ reservation }: { reservation: Reservations }) {
             <p className="truncate text-lg font-bold tracking-tight tablet:text-xl">
               {reservation.activity.title}
             </p>
-            <p className="tablet::text-2lg truncate text-md">
+            <p className="truncate text-md tablet:text-2lg">
               {reservation.date} · {reservation.startTime} - {reservation.endTime} ·{" "}
               {reservation.headCount}명
             </p>
           </div>
-          <div className="border-gary-200 border-t border-solid" />
+          <div className="border-t border-solid border-gray-200" />
           <p className="text-xl font-bold tablet:text-3xl">
             ₩{reservation.totalPrice.toLocaleString()}
           </p>
         </div>
       </div>
       <Rating onRatingChange={rate => setRating(rate)} currentRating={rating} />
-      <div className="border-gary-200 flex h-[40vh] w-full grow flex-col rounded border border-solid p-4 tablet:h-[240px]">
+      <div className="flex h-[40vh] w-full grow flex-col rounded border border-solid border-gray-200 p-4 tablet:h-[240px]">
         <textarea
           className="text-base scrollbar-hide h-full min-h-[150px] w-full flex-grow resize-none outline-none"
           placeholder="후기를 작성해 주세요."
